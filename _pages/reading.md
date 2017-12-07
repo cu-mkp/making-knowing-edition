9991ab232f746d6479f1bbda827027a4ac078e04
Nick Laiacona <nick@performantsoftware.com>
2017-12-07 17:11:26 -0500

    add open seadragon

---
+++ _pages/reading.md
@@ -0,0 +1,14 @@
+---
+permalink: /reading/
+layout: page
+title:  Reading Prototype
+date:   2015-01-03 21:21
+---
+
+<div>
+
+
+<div id="openseadragon1" style="width: 800px; height: 600px;"></div>
+
+
+</div>
--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,11 @@ $(document).foundation();
 //
 // Custom JS
 // --------------------------------------------------
+
+$( document ).ready(function() {
+  var viewer = OpenSeadragon({
+      id: "openseadragon1",
+      prefixUrl: "../assets/img/openseadragon/",
+      tileSources: "/path/to/my/image.dzi"
+  });
+});
