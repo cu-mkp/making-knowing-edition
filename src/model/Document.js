843ae364ebb71b9fc028aef4c77defe5a999c96c
Nick Laiacona <nick@performantsoftware.com>
2018-01-24 17:21:53 -0500

    use bnf thumbs

--- src/model/Document.js
+++ src/model/Document.js
@@ -6,37 +6,37 @@ class Document {
         id: 0,
         name: '3r',
         image_zoom_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f11/info.json',
-        image_thumbnail_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f11/full/100,/0/native.jpg'
+        image_thumbnail_url: 'http://gallica.bnf.fr/ark:/12148/btv1b10500001g/f11.thumbnail/full/native.jpg'
       },
       {
         id: 1,
         name: '3v',
         image_zoom_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f12/info.json',
-        image_thumbnail_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f12/full/100,/0/native.jpg'
+        image_thumbnail_url: 'http://gallica.bnf.fr/ark:/12148/btv1b10500001g/f12.thumbnail/full/native.jpg'
       },
       {
         id: 2,
         name: '4r',
         image_zoom_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f13/info.json',
-        image_thumbnail_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f13/full/100,/0/native.jpg'
+        image_thumbnail_url: 'http://gallica.bnf.fr/ark:/12148/btv1b10500001g/f13.thumbnail/full/native.jpg'
       },
       {
         id: 3,
         name: '4v',
         image_zoom_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f14/info.json',
-        image_thumbnail_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f14/full/100,/0/native.jpg'
+        image_thumbnail_url: 'http://gallica.bnf.fr/ark:/12148/btv1b10500001g/f14.thumbnail/full/native.jpg'
       },
       {
         id: 4,
         name: '5r',
         image_zoom_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f15/info.json',
-        image_thumbnail_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f15/full/100,/0/native.jpg'
+        image_thumbnail_url: 'http://gallica.bnf.fr/ark:/12148/btv1b10500001g/f15.thumbnail/full/native.jpg'
       },
       {
         id: 5,
         name: '5v',
         image_zoom_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f16/info.json',
-        image_thumbnail_url: 'http://gallica.bnf.fr/iiif/ark:/12148/btv1b10500001g/f16/full/100,/0/native.jpg'
+        image_thumbnail_url: 'http://gallica.bnf.fr/ark:/12148/btv1b10500001g/f16.thumbnail/full/native.jpg'
       }
     ];
   }
