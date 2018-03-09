7c5c82174dd2c42596db598005a67d6a7535e7f8
Nick Laiacona <nick@performantsoftware.com>
2018-03-09 18:15:48 -0500

    wip on thumb scroll

--- src/view/ImageGridView.js
+++ src/view/ImageGridView.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './css/ImageGridView.css';
 
 class ImageGridView extends Component {
+
+  constructor () {
+      super();
+      this.loadIncrement = 20;
+      let thumbs = this.moreThumbs.bind(this);
+      let thumbCount = (thumbs.length > loadIncrement) ? loadIncrement : thumbs.length;
+      this.state = { thumbs: thumbs, thumbCount: thumbCount };
+  }
 
   onClickThumb = (id, e) => {
     let splitPaneView = this.props.splitPaneView;
@@ -11,23 +20,49 @@ class ImageGridView extends Component {
     splitPaneView.openFolio(otherSide, id, 'TranscriptionView');
   }
 
-  render() {
+  generateThumbs () {
     let folios = this.props.document.folios;
-    let thumbs = folios; //.slice(0,20); // TODO load based on scrolling
+    let count = this.state.divs.length;
+
+    let thumbs = folios.map( (folio) => (
+      <li className="thumbnail" key={folio.id}>
+        <figure><a id={folio.id} onClick={this.onClickThumb.bind(this,folio.id)}><img src={folio.image_thumbnail_url} alt={folio.name}/></a></figure>
+        <figcaption className="thumbnail-caption">{folio.name}</figcaption>
+      </li>
+    ));
+
+    return thumbs;
+  }
+
+  moreThumbs() {
+    let thumbCount = this.state.thumbCount + this.loadIncrement;
+    if( thumbs.length >= thumbCount ) {
+      this.setState({ thumbCount: thumbCount });
+      return this.state.thumbs.slice(0,thumbCount);
+    } else {
+      this.setState({ thumbCount: thumbs.length });
+      return this.state.thumbs;
+    }
+  }
+
+  hasMore() {
+    return (this.state.thumbs.length >= this.state.thumbCount)
+  }
+
+  render() {
     let hidden = ( this.props.drawerMode && !this.props.drawerOpen ) ? "hidden" : "";
     let style = { height: this.props.viewHeight, overflow: 'scroll' };
 
     return (
       <div id='image-grid-view' className={hidden} style={style}>
         <ul>
-        {
-          thumbs.map((thumb) => (
-            <li className="thumbnail" key={thumb.id}>
-              <figure><a id={thumb.id} onClick={this.onClickThumb.bind(this,thumb.id)}><img src={thumb.image_thumbnail_url} alt={thumb.name}/></a></figure>
-              <figcaption className="thumbnail-caption">{thumb.name}</figcaption>
-            </li>
-          ))
-        }
+          <InfiniteScroll
+            next={this.moreThumbs}
+            hasMore={true}
+            height={style.height}
+            loader={<h4>Loading...</h4>}>
+            {this.state.divs}
+          </InfiniteScroll>
         </ul>
       </div>
     );
