8bb9a89bdb167e1a29696b18bba9bb0a229a290a
Andrew Sempere <a@feralresearch.org>
2018-04-25 16:49:21 +0200

    Adds tooltips on zoomtool

--- src/view/ImageZoomControl.js
+++ src/view/ImageZoomControl.js
@@ -9,12 +9,12 @@ class ImageZoomControl extends React.Component {
 		let onZoomGrid=(this.props.navigationState.bookMode?null:this.props.onZoomGrid);
 		return (
 			<ul className="ImageZoomControl">
-			  <li><i id={in_id} className="zoom-in fas fa-plus-circle fa-2x"></i></li>
-			  <li><i onClick={this.props.onZoomFixed_1} className="zoom-3 fas fa-circle fa-2x"></i></li>
-			  <li><i onClick={this.props.onZoomFixed_2} className="zoom-2 fas fa-circle fa-lg"></i></li>
-			  <li><i onClick={this.props.onZoomFixed_3} className="zoom-1 fas fa-circle"></i></li>
-			  <li><i id={out_id}  className="zoom-out fas fa-minus-circle fa-2x"></i></li>
-			  <li className={this.props.navigationState.bookMode?'disabled':''}><i onClick={onZoomGrid} className="zoom-grid fas fa-th fa-2x"></i></li>
+			  <li><i title="Zoom In" id={in_id} className="zoom-in fas fa-plus-circle fa-2x"></i></li>
+			  <li><i title="Fixed Zoom 1" onClick={this.props.onZoomFixed_1} className="zoom-3 fas fa-circle fa-2x"></i></li>
+			  <li><i title="Fixed Zoom 2" onClick={this.props.onZoomFixed_2} className="zoom-2 fas fa-circle fa-lg"></i></li>
+			  <li><i title="Fixed Zoom 3" onClick={this.props.onZoomFixed_3} className="zoom-1 fas fa-circle"></i></li>
+			  <li><i title="Zoom Out" id={out_id}  className="zoom-out fas fa-minus-circle fa-2x"></i></li>
+			  <li className={this.props.navigationState.bookMode?'disabled':''}><i title="Return to grid mode (not available in book mode)" onClick={onZoomGrid} className="zoom-grid fas fa-th fa-2x"></i></li>
 			</ul>
 		);
 	}
