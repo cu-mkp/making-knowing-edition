bfed13a42b7084ab82f4ddf306871790a295cde8
Nick Laiacona <nick@performantsoftware.com>
2018-06-28 20:52:13 -0600

    give initial state files own folder

--- src/action/rootReducer.js
+++ src/action/rootReducer.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux';
 
 import {createReducer} from '../model/ReduxStore';
 import DocumentViewActions from './DocumentViewActions';
-import navigationInitialState from './navigationInitialState';
 import SearchActions from './SearchActions';
-import searchInitialState from './searchInitialState';
 import GlossaryActions from './GlossaryActions';
-import glossaryInitialState from './glossaryInitialState';
+
+import navigationInitialState from './initialState/navigationInitialState';
+import searchInitialState from './initialState/searchInitialState';
+import glossaryInitialState from './initialState/glossaryInitialState';
 
 export default function rootReducer() {
     return combineReducers({
