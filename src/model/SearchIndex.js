7e64ee73a9c065dd96b0234b141ad5aa9949030d
Nick Laiacona <nick@performantsoftware.com>
2018-07-17 13:51:21 -0400

    don't display results w score of zero

--- src/model/SearchIndex.js
+++ src/model/SearchIndex.js
@@ -57,11 +57,13 @@ class SearchIndex {
     let recipes = [];
 
     for( let result of results ) {
+      if( result.score > 0 ) {
       	let recipe = this.recipeBook[transcriptionType][ result.ref ];
-		if( recipe ) {
-	      let fragments = createFragments( result.matchData.metadata, recipe.content )
-	      recipes.push( { name: recipe.name, folio: recipe.folioID, contextFragments: fragments } );
-		}
+        if( recipe ) {
+            let fragments = createFragments( result.matchData.metadata, recipe.content )
+            recipes.push( { name: recipe.name, folio: recipe.folioID, contextFragments: fragments } );
+        }
+      }
     }
 
     return recipes;
