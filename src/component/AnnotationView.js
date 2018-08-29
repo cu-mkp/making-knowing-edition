3eb7ab7d4d79857d46dc20b1189da06fd594d3af
Nick Laiacona <nick@performantsoftware.com>
2018-08-29 10:15:11 -0400

    wip to implement footnote scroller

--- src/component/AnnotationView.js
+++ src/component/AnnotationView.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Parser from 'html-react-parser';
+import { Element, scroller } from 'react-scroll';
+ 
 
 import { dispatchAction } from '../model/ReduxStore';
 
@@ -18,12 +20,36 @@ class AnnotationView extends Component {
         dispatchAction( this.props, 'DiplomaticActions.setFixedFrameMode', false );
     }
 
+    scrollToAnchorTag = (e) => {                
+        // Somewhere else, even another file
+        scroller.scrollTo('myScrollToElement', {
+            duration: 500,
+            smooth: true,
+            containerId: this.id,
+            offset: 5 
+        }); 
+    }
+
     // Configure parser to replace certain tags with components
     htmlToReactParserOptions() {
 		var parserOptions =  {
 			 replace: function(domNode) {
-
 				 switch (domNode.name) {
+                    case 'a':
+                        // determine tag class
+                        const tagClass = null;
+                        if( tagClass === 'footnote-ref' || tagClass === 'footnote-back' ) {
+                            // href="#fnref32"
+                            return (
+                                // reconstruct the link
+                                <Element name="fnref32">
+                                    <a onClick={this.scrollToAnchorTag}></a>
+                                </Element>
+                            );
+                        }
+                        else {
+                            return domNode;
+                        }
 
 					 default:
 						 /* Otherwise, Just pass through */
