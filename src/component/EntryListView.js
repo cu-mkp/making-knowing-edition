cc2e8aff1dead3395d2abd06950cebd32d1821b9
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2019-10-02 12:10:59 -0700

    basic idea

--- src/component/EntryListView.js
+++ src/component/EntryListView.js
@@ -3,7 +3,10 @@ import {connect} from 'react-redux';
 import ReactList from 'react-list';
 import {Typography, Card, Chip, Avatar } from '@material-ui/core';
 import { CardContent, Link } from '@material-ui/core';
-
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { dispatchAction } from '../model/ReduxStore';
 
 class EntryListView extends Component {
@@ -13,30 +16,51 @@ class EntryListView extends Component {
     }
 
     renderEntryCard = (index, key) => {        
-        const { entryList, tagNameMap } = this.props.entries
-        const entry = entryList[index]
-
-        let tags = [];
-        for( let tagID of Object.keys(tagNameMap) ) {
-            if( entry.mentions[tagID] > 0 ) {
-                tags.push({ id: tagID, name: tagNameMap[tagID], count: entry.mentions[tagID]})
-            }
-        }
-        let mentionRow = ( tags.length > 0 ) ? this.renderCardChips(tags) : '';
-
-        const folioURL = `/folios/${entry.folio.replace(/^[0|\D]*/,'')}`
-
-        return(
+      const { entryList, tagNameMap } = this.props.entries
+      const entry = entryList[index];
+      let tags = [];
+      for( let tagID of Object.keys(tagNameMap) ) {
+          if( entry.mentions[tagID] > 0 ) {
+              tags.push({ id: tagID, name: tagNameMap[tagID], count: entry.mentions[tagID]})
+          }
+      }
+      let mentionRow = ( tags.length > 0 ) ? this.renderCardChips(tags) : '';
+     
+      const folioURL = `/folios/${entry.folio.replace(/^[0|\D]*/,'')}`
+      let chips = this.renderCardChips(tags)
+      let references = this.renderReferences(tags, entry)
+      return(
             <Card className="entry" key={key}>
-                <CardContent>
-                    <Link onClick={e => {this.props.history.push(folioURL)}} ><Typography variant="h6">{`${entry.displayHeading} - ${entry.folio}`}</Typography></Link>
-                    <Typography>Moldmaking and Metalworking</Typography>
-                    <Typography>Annotations: <i>Too thin things, fol. 142v (Fu, Zhang)</i></Typography>
-                    <div className="entry-chips">{mentionRow}</div>
-                </CardContent>
-            </Card>
-        )
-    }
+            <CardContent>
+                    <ExpansionPanel >
+                          <ExpansionPanelSummary  >
+                          <div style={{display:'flex', flexDirection:'column'}}>
+                              <Link onClick={e => {this.props.history.push(folioURL)}} ><Typography variant="h6">{`${entry.displayHeading} - ${entry.folio}`}</Typography></Link>
+                              <Typography>Moldmaking and Metalworking</Typography>
+                              <Typography>Annotations: <i>Too thin things, fol. 142v (Fu, Zhang)</i></Typography>
+                              <div className="entry-chips">{mentionRow}</div>
+                        </div>        
+                        </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                        <div style={{display:'flex',flexDirection:'column',width:'100%'}}>
+                              {
+                                    tags.map((tag,index)=>{
+                                          return (
+                                              <div style={{display:'flex',marginBottom:'5px',marginTop:'5px'}}> 
+                                                      <div style={{width:'30%'}}> {chips[index]} </div> 
+                                                      <div style={{width:'70%' }}>{entry.text_references[tag.id]} </div>
+                                                </div>
+                                          )
+                                    })
+                              }
+                        
+                        </div>
+                  </ExpansionPanelDetails>
+                  </ExpansionPanel>
+            </CardContent>
+            </Card>  
+      )
+  }
 
     onClickNavigationChip = (e) => {
         const tagID = e.currentTarget.getAttribute('tagid')
@@ -71,6 +95,15 @@ class EntryListView extends Component {
         )
     }
 
+    renderReferences(tags, entry){
+     let references = [];
+     for( let tag of tags ){
+           references.push( <div>{entry.text_references[tag.id]}</div>)
+     }
+   return references;
+    }
+
+    
     renderNavigationChips(tags) {
         const { filterTags } = this.props.entries
 
