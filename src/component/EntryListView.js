69d0d9d685480c5b2ffb85ccb75c045ba92ed5a6
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2019-10-08 12:36:10 -0700

    restore mention row reference

--- src/component/EntryListView.js
+++ src/component/EntryListView.js
@@ -44,12 +44,12 @@ class EntryListView extends Component {
             const folioURL = `/folios/${entry.folio.replace(/^[0|\D]*/,'')}`
             return (
                         <ExpansionPanel className="entry" key={entry.id} onChange={this.toggleIconButton}>
-                              <ExpansionPanelSummary   expandIcon={<div><ExpandMoreIcon className="colapse-button" /></div>}>
+                              <ExpansionPanelSummary   expandIcon={ tags.length >0 ? (<div><ExpandMoreIcon className="colapse-button" /></div>):''}>
                                     <div className={"detail-container"}>
                                           <Link onClick={e => {this.props.history.push(folioURL)}} ><Typography variant="h6">{`${entry.displayHeading} - ${entry.folio}`}</Typography></Link>
                                           <Typography>Moldmaking and Metalworking</Typography>
-                                    <Typography>Annotations: <i>Too thin things, fol. 142v (Fu, Zhang)</i></Typography>
-                                          <div className="entry-chips">{this.props.mentionRow}</div>
+                                          <Typography>Annotations: <i>Too thin things, fol. 142v (Fu, Zhang)</i></Typography>
+                                          <div className="entry-chips">{mentionRow}</div>
                                     </div>        
                         </ExpansionPanelSummary>
 
