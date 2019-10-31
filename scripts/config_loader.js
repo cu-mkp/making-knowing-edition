1e83effef0023e36f762a11d4c237df738acb365
Nick Laiacona <nick@performantsoftware.com>
2019-10-31 13:28:58 -0400

    fix config file path

--- scripts/config_loader.js
+++ scripts/config_loader.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function load() {
    console.log(`fs relative paths to current working directory`,process.cwd())
-    const configJSON = fs.readFileSync('./making-knowing-edition/edition_data/config.json', "utf8");
+    const configJSON = fs.readFileSync('edition_data/config.json', "utf8");
     return JSON.parse(configJSON);
 }
 
