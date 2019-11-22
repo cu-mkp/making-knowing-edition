0df80daf25c315f7af88f6fec1f18501f64945c4
Nick Laiacona <nick@performantsoftware.com>
2019-11-22 18:20:21 -0500

    add eco config

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps : [
+        {
+            name      : 'asset_server',
+            script    : 'scripts/asset_server.js',
+            log       : 'edition_data/working/pm2_logfile.log'
+        },
+        {
+            name      : 'lizard',
+            script    : 'scripts/lizard.js run',
+            log       : 'edition_data/working/lizard_pm2.log'
+        }
+    ]
+};
