5e4aca235d8690e7bea635647666181681fdee1b
Nick Laiacona <nick@performantsoftware.com>
2020-02-28 08:29:39 -0500

    SEO for production and production mode flag
    * add release mode flag
    
    * edition sitemap and google seo tag

--- src/action/initialState/diplomaticInitialState.js
+++ src/action/initialState/diplomaticInitialState.js
@@ -1,5 +1,6 @@
 export default {
     fixedFrameMode: false,
     firstPageLoad: true,
+    releaseMode: process.env.REACT_APP_RELEASE_MODE,
     googleAnalyticsTrackingID: process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID
 }	
