a010d6da2f73ecd65e9332394b530084ea457ba9
Nick Laiacona <nick@performantsoftware.com>
2020-04-30 09:26:38 -0400

    improve layout of annotation

--- src/component/AnnotationView.js
+++ src/component/AnnotationView.js
@@ -6,7 +6,6 @@ import { Link } from 'react-scroll';
 import { Link as ReactLink } from 'react-router-dom';
 import { dispatchAction } from '../model/ReduxStore';
 import FigureImage from './FigureImage'
-import AnnotationByLine from './AnnotationByLine';
 
 class AnnotationView extends Component {
 
@@ -37,6 +36,27 @@ class AnnotationView extends Component {
         else {
             return domNode;
         }
+    }
+
+    renderByLine(annoAuthors, authors) {
+        if( !annoAuthors ) return null
+
+        const authorEntries = []
+        for( const annoAuthor of annoAuthors ) {
+            const { fullName, semester, year, authorType, degree, yearAtTime, department } = authors[annoAuthor]
+            const bylineArray = [ fullName, semester, year, authorType, degree, yearAtTime, department ].filter( a => (a && a.length > 0) )
+            const byline = bylineArray.join(', ')
+            authorEntries.push(
+                <p>{byline}</p>
+            )
+        }
+
+        return (
+            <div className="anno-byline">
+                { authorEntries }
+                <p><a href="#">doi link</a></p>
+            </div>
+        )
     }
 
     // Configure parser to replace certain tags with components
@@ -76,27 +96,15 @@ class AnnotationView extends Component {
                 if ( domNode.name === 'h1' ) {
                     const isAbstract = (!anno.abstract || anno.abstract.length === 0) ? false : true;
                     const title = domToReact([domNode])
-                    const byLine = domToReact([domNode.next.next])
                     return (
                         <div>
                             <div className="title-byline-container">
                                 {title}
-                                {
-                                    // TODO: Commented out the below AnnotationByline code to replace byLine code  
-                                    //       derrived from google docs once by lines are removed from google docs
-                                    //       (see comments in issue: https://github.com/cu-mkp/making-knowing-edition/issues/394)
-                                    // anno.authors ?
-                                    //     <div style={{marginBottom: 30}}>
-                                    //         <AnnotationByLine annoAuthors={anno.authors} authors={authors} />
-                                    //     </div>
-                                    //     :
-                                    //     ""
-                                }
-                                {byLine} 
+                                { this.renderByLine(anno.authors, authors) }
                             </div>
                             {isAbstract &&
                                 <div className="annotation-abstract">
-                                    <h2 style={{marginTop: 0}}>Abstract</h2>
+                                    <h2>Abstract</h2>
                                     {Parser(anno.abstract)}
                                 </div>
                             }
@@ -137,7 +145,7 @@ class AnnotationView extends Component {
         if( this.props.inSearchMode ) return '';
         return (
             <div className='annotation-nav'>
-                <ReactLink to='/essays'>Back to List</ReactLink>
+                <ReactLink to='/essays'><i className="fa fa-2x fa-arrow-circle-left"></i></ReactLink>
             </div>
         );
     }
