18cba0e55d472433caff1ba5f406e43989149d0c
Nick Laiacona <nick@performantsoftware.com>
2020-08-26 11:44:02 -0400

    add reading guide

--- src/component/HelpPopper.js
+++ src/component/HelpPopper.js
@@ -17,11 +17,9 @@ const HelpPopper=(props)=>{
                               <div onClick={props.onClose} className="closeX">
                                     <span className="fa fa-window-close" ></span>
                               </div>
-                        <div className="helpHeader">
-                              <Typography variant="subtitle1">Toolbar Buttons</Typography>
-                        </div>
-                        <div>
-                              <List>
+                              <div>
+                                    <Typography variant="h6" component="h2">Toolbar Buttons</Typography>
+                                    <List>
                                           <ListItem >
                                                 <span className='fa fa-lock active'></span>
                                                 <ListItemText primary="Toggle Sync Views" />
@@ -35,10 +33,6 @@ const HelpPopper=(props)=>{
                                                 <ListItemText primary="Toggle XML Mode" />
                                           </ListItem>
                                           <ListItem >
-                                                <span className='fa fa-columns active'></span>
-                                                <ListItemText primary="Toggle Single Column Mode" />
-                                          </ListItem>
-                                          <ListItem >
                                           <span><Icon.ArrowCircleLeft/><Icon.ArrowCircleRight/></span>
                                                 <ListItemText primary="Go Forward / Back" />
                                           </ListItem>
@@ -46,9 +40,42 @@ const HelpPopper=(props)=>{
                                                 <span className='fa fa-hand-point-right active'></span>
                                                 <ListItemText primary="Jump to folio" />
                                           </ListItem>
+                                          <div className="readingGuide">
+                                                <Typography variant="h6" component="h2">Reading Guide</Typography>
+                                                <table>
+                                                      <thead>
+                                                      <tr>
+                                                            <th><Typography variant="overline" component="span">Syntax</Typography></th>
+                                                            <th><Typography variant="overline" component="span">Meaning</Typography></th>
+                                                      </tr>
+                                                      </thead>
+                                                      <tbody>
+                                                            <tr>
+                                                                  <td><img src="/img/howtouse-ups.png" alt="howtouse-ups"/></td>
+                                                                  <td><Typography>Text is under the paper strip</Typography></td>
+                                                            </tr>
+                                                            <tr>
+                                                                  <td><img src="/img/howtouse-curly.png" alt="howtouse-curly"/></td>
+                                                                  <td><Typography>Expanded version implied by abbreviation marks</Typography></td>
+                                                            </tr>
+                                                            <tr>
+                                                                  <td><img src="/img/howtouse-square.png" alt="howtouse-square"/></td>
+                                                                  <td><Typography>Editorial interventions and corrections</Typography></td>
+                                                            </tr>
+                                                            <tr>
+                                                                  <td><img src="/img/howtouse-beaker.png" alt="howtouse-beaker"/></td>
+                                                                  <td><Typography>Associated research essay</Typography></td>
+                                                            </tr>
+                                                            <tr >
+                                                                  <td><img src="/img/howtouse-asterisk.png" alt="howtouse-asterisk"/></td>
+                                                                  <td><Typography>Editorial Comments</Typography></td>
+                                                            </tr>
+                                                      </tbody>
+                                                </table>
+                                          </div>
                                     </List>
-                              </div>
-                             
+                                    <Typography>See <a href="#/content/how-to-use">How to Use</a> for more information.</Typography>
+                              </div>                             
                         </Paper>
                         </Fade>
       </Popper>)
